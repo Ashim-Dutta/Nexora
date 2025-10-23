4045aaf3680988aa677ff60ea49ed1c0c60457f8
Ashim Dutta <ashimdutta800@gmail.com>
2025-10-23 20:06:07 +0530

    payment service is completed

--- payment/src/routes/payment.routes.js
+++ payment/src/routes/payment.routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const createAuthMiddleware = require('../middlewares/auth.middleware')
+const paymentController = require('../controllers/payment.controller')
 const router = express.Router()
 
 
-router.post('/',createAuthMiddleware(['user']))
+router.post('/create/:orderId', createAuthMiddleware(['user']), paymentController.createPayment)
+
+router.post('/verify',createAuthMiddleware(['user']),paymentController.verifyPayment)
 
 
 module.exports = router
